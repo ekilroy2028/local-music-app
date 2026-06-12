5f815e382ab0bdf0697a59d7a85686c99ae2bb52
ekilroy2028 <ekilroy1@asu.edu>
2026-06-12 11:59:15 -0400

    debug- site not seeing backend
    It's seeing the front end, but it's just not It's seeing the front end, but it's just not

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
